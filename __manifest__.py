e4bcbdf6bf4b9a458fcde8ec3aeafbe45ce13b35
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-10-19 21:23:54 +0700

    Update 20221019

--- __manifest__.py
+++ __manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "PT. Ismata Nusantara Abadi",
+    'website': "https://ismata.co.id",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,11 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','sale'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
+        'security/security.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from unicodedata import name
 from odoo import models, fields, api
 
 class ResPartner(models.Model):
@@ -67,24 +68,14 @@ class ResPartner(models.Model):
 class TravelPackage(models.Model):
     _name = 'travel.package'
     _description = 'Travel Package'
-
-    # Quota Onchange
-    @api.onchange('quota')
-    def _onchange_quota(self):
-        self.remaining_quota = self.quota
-    
-    # Package Onchange
-    @api.onchange('package_id')
-    def _onchange_package_id(self):
-        pass
     
     # Informasi Jadwal
     tanggal_berangkat = fields.Date('Tanggal berangkat', required=True)
     tanggal_kembali = fields.Date('Tanggal Kembali', required=True)
     
     # Produk
-    sale_id = fields.Many2one('product.product', string='Sale')
-    package_id = fields.Many2one('product.product', string='Package')
+    product_sale_id = fields.Many2one('product.product', string='Sale', required=True)
+    product_package_id = fields.Many2one('product.product', string='Package', required=True)
 
     # Seats
     quota = fields.Integer('Quota')
@@ -92,28 +83,68 @@ class TravelPackage(models.Model):
     quota_progress = fields.Float('Quota Progress')
 
     # Lines
-    hotels_line = fields.One2many('hotels.lines', 'hotels_id', string='Hotels Line')
-    airline_line = fields.One2many('airline.lines', 'airline_id', string='Airline Lines')
-    schedule_line = fields.One2many('schedule.lines', 'schedule_id', string='Schedule Lines')
-    manifest_line = fields.One2many('manifest.lines', 'manifest_id', string='Manifest Lines')
-    hpp_line = fields.One2many('hpp.lines', 'hpp_id', string='HPP Lines')
+    hotels_line = fields.One2many('hotels.lines', 'travel_id', string='Hotels Line')
+    airline_line = fields.One2many('airline.lines', 'travel_id', string='Airline Lines')
+    schedule_line = fields.One2many('schedule.lines', 'travel_id', string='Schedule Lines')
+    manifest_line = fields.One2many('manifest.lines', 'travel_id', string='Manifest Lines')
+    hpp_line = fields.One2many('hpp.lines', 'travel_id', string='HPP Lines')
+
+    total = fields.Float(compute='_compute_total', string='Total')
+
+    # Sequence Travel Package
+    name = fields.Char(string='Referensi', readonly=True, default='/')
+    @api.model
+    def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].next_by_code('travel.package')
+        return super(TravelPackage, self).create(vals)
+
+     # Onchange Quota
+    @api.onchange('quota')
+    def _onchange_quota(self):
+        self.remaining_quota = self.quota
+
+    # Compute Total
+    @api.depends('hpp_line.subtotal')
+    def _compute_total(self):
+        for rec in self:
+            rec.total = 0
+            for i in rec.hpp_line:
+                print('PRINT ==============>',i.subtotal)
+                rec.total += i.subtotal
+                print('TOTAL : ', rec.total)
+
+    # Onchange Package
+    @api.onchange('product_package_id')
+    def _onchange_product_package_id(self):
+        for rec in self:
+            lines = [(5,0,0)]
+            for line in rec.product_package_id.bom_ids.bom_line_ids:
+                vals = {
+                    'travel_id' : line.id,
+                    'barang' : line.display_name,
+                    'quantity' : line.product_qty,
+                    'unit' : line.product_uom_id.id,
+                    'unit_price' : line.product_id.standard_price,
+                }
+                lines.append((0, 0, vals))
+            rec.hpp_line = lines
 
 class HotelsLines(models.Model):
     _name = 'hotels.lines'
     _description = 'Hotels Lines'
 
-    hotels_id = fields.Many2one('travel.package', string='Hotels Lines')
-    hotels_name = fields.Many2one('res.partner', string='Nama Hotel', required=True, domain=[('hotels', '=', True)])
+    travel_id = fields.Many2one('travel.package', string='Hotels Lines')
+    partner_id = fields.Many2one('res.partner', string='Nama Hotel', required=True, domain=[('hotels', '=', True)])
     check_in = fields.Date('Check In Hotel', required=True)
     check_out = fields.Date('Check Out Hotel', required=True)
-    kota = fields.Char(string='Kota', related='hotels_name.city')
+    kota = fields.Char(string='Kota', related='partner_id.city')
 
 class AirlineLines(models.Model):
     _name = 'airline.lines'
     _description = 'Airline Lines'
 
-    airline_id = fields.Many2one('travel.package', string='Airline Lines')
-    airline_name = fields.Many2one('res.partner', string='Nama Airline', required=True, domain=[('airlines', '=', True)])
+    travel_id = fields.Many2one('travel.package', string='Airline Lines')
+    partner_id = fields.Many2one('res.partner', string='Nama Airline', required=True, domain=[('airlines', '=', True)])
     tanggal_berangkat = fields.Date('Tanggal Berangkat', required=True)
     kota_asal = fields.Char('Kota Asal', required=True)
     kota_tujuan = fields.Char('Kota Tujuan', required=True)
@@ -122,7 +153,7 @@ class ScheduleLines(models.Model):
     _name = 'schedule.lines'
     _description = 'Schedule Lines'
 
-    schedule_id = fields.Many2one('travel.package', string='Schedule Lines')
+    travel_id = fields.Many2one('travel.package', string='Schedule Lines')
     schedule_name = fields.Char(string='Nama Kegiatan',required=True)
     tanggal_kegiatan = fields.Date('Tanggal Kegiatan', required=True)
 
@@ -130,7 +161,7 @@ class ManifestLines(models.Model):
     _name = 'manifest.lines'
     _description = 'Manifest Lines'
 
-    manifest_id = fields.Many2one('travel.package', string='Manifest Lines')
+    travel_id = fields.Many2one('travel.package', string='Manifest Lines')
     title = fields.Char('Title')
     nama_passpor = fields.Char('Nama Passpor')
     jenis_kelamin = fields.Char('Jenis Kelamin')
@@ -154,9 +185,17 @@ class HPPLines(models.Model):
     _name = 'hpp.lines'
     _description = 'HPP Lines'
 
-    hpp_id = fields.Many2one('travel.package', string='HPP Lines')
+    travel_id = fields.Many2one('travel.package', string='HPP Lines')
     barang = fields.Char('Barang')
     quantity = fields.Integer('Quantity')
-    unit = fields.Char('Unit(s)')
+    unit = fields.Many2one('uom.uom', string='Unit(s)')
     unit_price = fields.Float('Unit Price')
-    subtotal = fields.Float('Subtotal')
+    subtotal = fields.Float(compute='_compute_subtotal', string='Subtotal')
+    
+    # Compute Subtotal
+    @api.depends('quantity')
+    def _compute_subtotal(self):
+        for rec in self:
+            rec.subtotal = 0
+            if rec.quantity and rec.unit_price:
+                rec.subtotal = rec.quantity * rec.unit_price
